b56f37dad1e00775eb054e2f4abc15748fcbf372
Harsh Patel <coderharshpatel@gmail.com>
2016-09-01 17:38:09 -0400

    connect mysql database

---
+++ app.js
@@ -0,0 +1,36 @@
+var mysql      = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : '127.0.0.1',
+  user     : 'root',
+  password : 'root',
+  database : 'inventory'
+});
+
+connection.connect(function(err) {
+	if (err) console.log(err);
+	else console.log("Connection sucessful!");
+});
+
+connection.query('SELECT * from ecommerce_phone', function(err, rows, fields) {
+  if (err) throw err;
+  else console.log(fields);
+});
+
+
+var post = ;
+
+connection.query('INSERT INTO ecommerce_phone (fields ...) VALUES (values...)', post, function(err, result) {
+
+});
+
+connection.end();
+
+
+
+//reading and writing into mysql from json
+/* 
+INSERT INTO table_name ( field1, field2,...fieldN )
+                       VALUES
+                       ( value1, value2,...valueN );
+*/
