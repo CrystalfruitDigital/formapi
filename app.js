824e66baabf619b1074b93af7a6ae0136aefc1eb
Harsh Patel <coderharshpatel@gmail.com>
2016-09-02 19:00:40 -0400

    Fix added json commas for part_name.json

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 var mysql      = require('mysql');
+var fs         = require('fs');
+// var phones     = require('json.json');
+
+var colors = ["Black", "White", "Red", "Blue", "Yellow", "Pink", "Purple", "Gray", "Brown", "Green", "Orange"];
+var contents = JSON.parse(fs.readFileSync("part_name.json", "utf8"));
+
 
 var connection = mysql.createConnection({
   host     : '127.0.0.1',
@@ -9,12 +15,15 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
 	if (err) console.log(err);
-	else console.log("Connection sucessful!");
+	else {
+		console.log("Connection sucessful!");
+		console.log(JSON.stringify(contents));
+	}
 });
 
 connection.query('SELECT * from ecommerce_phone', function(err, rows, fields) {
   if (err) throw err;
-  else console.log(fields);
+  // else console.log(fields);
 });
 
 
